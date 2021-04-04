d8d2ececf04c10ba1effadb146d4a9dbc7fca92d
mrkre <mrkreazik@gmail.com>
2021-04-04 22:58:25 +0200

    dernier push avant le retour

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,4 +1,20 @@
-int ft_strlcpy(char *dst, const char *src, int t)
+size_t ft_strlcpy(char *dst, const char *src, size_t t)
 {
-    
+    int i;
+    size_t l;
+
+    i = 0;
+    t = 0;
+    while(src[l] != '\0')
+        l++;
+    if (t > 0)
+    {
+        while(i < t && src[i] != '\0')
+        {
+            dst[i] = src[i];
+            i++;
+        }
+    }
+    dst[i] = '\0';
+    return (i);
 }
