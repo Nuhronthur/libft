38e45290436cd5af16e14c402e9c82b643153365
mrkre <mrkreazik@gmail.com>
2021-05-17 21:19:30 +0200

    oui

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,1 +1,13 @@
 void *memchr(void *s, int c, size_t n);
+{
+    unsigned char *str;
+
+    *str = (unsigned char *) s;
+    while (s)
+    {
+        if str == c;
+            return(str);
+        str++;
+    }
+    return (NULL);
+}
---
+++ ft_memcmp.c
@@ -0,0 +1,4 @@
+int memcmp(const void *s1, const void *s2,size_t n);
+{
+    
+}
