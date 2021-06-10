ce39d78594c04616247d262e6b7cd3a89bf22113
Jonathan Vella <jovella@c2r12p3.42nice.fr>
2021-06-10 16:44:54 +0200

    raf9

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jovella <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 12:37:22 by jovella           #+#    #+#             */
-/*   Updated: 2021/06/10 12:44:37 by jovella          ###   ########.fr       */
+/*   Updated: 2021/06/10 16:05:30 by jovella          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*calloc(size_t l, size_t s)
 		l = 1;
 		s = 1;
 	}
-	t = malloc(l * s)
+	t = malloc(l * s);
 		if (t == NULL)
 		return (NULL);
 	ft_bzero(t, l * s);
---
+++ ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovella <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/10 15:12:11 by jovella           #+#    #+#             */
+/*   Updated: 2021/06/10 16:40:28 by jovella          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char const	*c;
+	size_t		i;
+
+	i = ft_strlen(s);
+	if (!s)
+		return (NULL);
+	if (len > i || (len + start > i && start < i))
+		len = i - start;
+	if (start >= i)
+		len = 0;
+	c = (char *)ft_calloc((len + 1), sizeof(*c));
+	if (!c)
+		return (NULL);
+	ft_memcpy(c, s, len);
+	return (c);
+}
