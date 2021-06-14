d38e055411be16a23088bbf2fccacdc7f6a72c75
nuhronthur <mrkreazik@gmail.com>
2021-06-14 16:32:49 +0200

    il en reste 8

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -16,9 +16,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
     size_t  i;
     size_t  j;
-    size_t  k;
+    char    *k;
 
     i = ft_strlen(s1);
     j = ft_strlen(s2);
-    k = i + j;
+    if (s1 == NULL || s2 == NULL)
+        return (NULL);
+    k = (char *)ft_calloc((i + j), sizeof(k));
+    if (k == NULL)
+        return (NULL);
+    ft_memcpy(k, s1, i);
+    ft_memcpy((k + i), s2, j);
+    return (k);
 }
