17305d61595549d1e43e6afa966a9f57ba915c1d
Jonathan Vella <jovella@c3r2p2.42nice.fr>
2021-06-21 18:25:31 +0200

    yo

--- ft_freedom.c
+++ ft_freedom.c
@@ -6,13 +6,13 @@
 /*   By: jovella <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 13:12:59 by jovella           #+#    #+#             */
-/*   Updated: 2021/06/21 14:24:20 by jovella          ###   ########.fr       */
+/*   Updated: 2021/06/21 18:17:41 by jovella          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	**ft_freedom(char const **dst, int j)
+char	**ft_freedom(char const **dst, int j)
 {
 	while (j > 0)
 	{
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jovella <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 13:01:12 by jovella           #+#    #+#             */
-/*   Updated: 2021/06/21 15:04:39 by jovella          ###   ########.fr       */
+/*   Updated: 2021/06/21 18:24:24 by jovella          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static char		**affect(char const *s1, char **dst, char c, int l);
 int				ft_atoi(const char *s);
 void			ft_bzero(void *s, size_t n);
 void			*calloc(size_t l, size_t s);
-static char		**ft_freedom(char const **dst, int j);
+char			**ft_freedom(char const **dst, int j);
 int				ft_intisn(int n);
 size_t			ft_intl(int c);
 int				ft_isalnum(int c);
@@ -41,7 +41,7 @@ int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t l);
 void			*ft_memmove(void *dest, const void *src, size_t n);
 void			*ft_memset(void *s, int c, size_t n);
-static int		ft_numchar(char const *s1, char c, int i);
+int				ft_numchar(char const *s1, char c, int i);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
