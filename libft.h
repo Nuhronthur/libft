e51858a3cebfb912c86aa26a6280c00ef2365070
nuhronthur <mrkreazik@gmail.com>
2021-08-15 14:09:48 +0200

    la fin

--- libft.h
+++ libft.h
@@ -51,6 +51,7 @@ void			ft_putstr_fd(char *s, int fd);
 char			**ft_split(char const *s, char c);
 char			*ft_strchr(const char *s, int c);
 char			*strdup(const char *s);
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 char			*ft_strjoin(char const *s1, char const *s2);
 size_t			ft_strlcat(char *dst, const char *src, size_t l);
 size_t			ft_strlcpy(char *dst, const char *src, size_t t);
