93470fb5b8a31ba391dbde841a15e5591cf3433e
nuhronthur <mrkreazik@gmail.com>
2021-08-15 04:45:33 +0200

    oui

--- ft_freedom.c
+++ ft_freedom.c
@@ -6,17 +6,13 @@
 /*   By: jovella <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 13:12:59 by jovella           #+#    #+#             */
-/*   Updated: 2021/06/21 18:17:41 by jovella          ###   ########.fr       */
+/*   Updated: 2021/06/21 14:24:20 by jovella          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-<<<<<<< HEAD
 static int	ft_freedom(char **t, int i)
-=======
-char	**ft_freedom(char const **dst, int j)
->>>>>>> 17305d61595549d1e43e6afa966a9f57ba915c1d
 {
 	if (!t[i])
 	{
